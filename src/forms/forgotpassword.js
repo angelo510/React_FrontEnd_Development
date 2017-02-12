514d2f7adbd573365cfb9d86d2243dfd4e7fe7af
Angelo510 <re.se.t@outlook.com>
2017-02-12 18:25:50 +0100

    Changed Password Form

--- src/forms/forgotpassword.js
+++ src/forms/forgotpassword.js
@@ -10,29 +10,6 @@ const ForgotPasswordForm = (props) => {
       <fieldset className="form-group">
         <h2 style={{textAlign: 'center', color: 'white'}}><Text text="modals.login.forgot_your_password" /></h2>
       </fieldset>
-
-      <div className="flex-row">
-        <Field
-          name="email"
-          component="input"
-          type="email"
-          placeholder="Email"
-          className="input-text flex" />
-      </div>
-
-
-      <fieldset className="form-group form-group_btns">
-        <LaddaButton
-          data-style={SLIDE_UP}
-          loading={buttonState}
-          type="submit"
-          disabled={pristine || submitting}
-          className="btn btn-primary btn-block"
-          style={{height: '50px', backgroundColor: '#0e7dff', borderColor: '#0e7dff'}}>
-            <Text text="modals.login.reset_password_instructions" />
-        </LaddaButton>
-      </fieldset>
-    </form>
   )
 }
 
